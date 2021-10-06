b6adcfdf1296d0f47bea36281073ff2a2fd07354
Michal <michalgrzegorczyk04@gmail.com>
2021-10-06 19:14:15 +0200

    Add first component and props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+	const expenses = [
+		{ id: 'e1', title: 'Xero paper', amount: 12.64, date: new Date(2021, 2, 28) },
+		{ id: 'e2', title: 'New Car', amount: 1299.22, date: new Date(2021, 2, 28) },
+		{ id: 'e3', title: 'Car Insurance', amount: 120.24, date: new Date(2021, 2, 28) },
+		{ id: 'e4', title: 'New desk', amount: 98, date: new Date(2021, 2, 28) },
+	];
+
 	return (
 		<div>
-			<h2>Let's get started!</h2>
-			<p>This is a paragraph!</p>
+			<h2>Title</h2>
+			<ExpenseItem 
+				title={expenses[0].title} 
+				amount={expenses[0].amount} 
+				date={expenses[0].date}
+			></ExpenseItem>
+			<ExpenseItem 
+				title={expenses[1].title} 
+				amount={expenses[1].amount} 
+				date={expenses[1].date}
+			></ExpenseItem>
+			<ExpenseItem 
+				title={expenses[2].title} 
+				amount={expenses[2].amount} 
+				date={expenses[2].date}
+			></ExpenseItem>
+			<ExpenseItem 
+				title={expenses[3].title} 
+				amount={expenses[3].amount} 
+				date={expenses[3].date}
+			></ExpenseItem>
 		</div>
 	);
 }
